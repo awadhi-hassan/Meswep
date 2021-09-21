20282b292f80b9103baf5e26a7f62f0bf7f2ca41
awadhi-hassan <awadhhassan77@gmail.com>
2021-09-21 13:17:35 +0300

    added about and contact pages

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,17 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <strong>Muslim Education Secretariat-Western Province</strong>
+                    <div class="mt-2">
+                        Muslim Education Secretariat-Western Province (MESWEP) is the official representative of Muslim Education Council (MEC)
+                        in Kakamega, Bungoma, Busia and Vihiga and consequently, the official sponsor of all Muslim Educational Institutions in
+                         this region in accordance with Article 53 of the Constitution of Kenya.         
+                    </div>
+                    <div class="slideshow flex my-5" style="justify-content:center;">
+                        <img src="{{ asset('previous.svg') }}" style="border-radius:10px;">
+                        <img src="{{ asset('media/kakamega1.jfif') }}" style="border-radius:10px;">
+                        <img src="{{ asset('next.svg') }}">
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,6 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <!--<x-application-logo class="block h-10 w-auto fill-current text-gray-600" />-->
                         <img src="{{ asset('logo.png') }}" style="height:40px; width:auto;">
                     </a>
                 </div>
